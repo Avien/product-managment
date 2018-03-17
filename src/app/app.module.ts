40b7ca38e04cca1a5dfc46ec7217360b2bf1a85a
Avien <avi.engelstein@gmail.com>
2018-03-17 20:19:10 +0200

    search pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './components/products/products.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
-import {AppRoutingModule} from './app.routes';
-import {ProductService} from './services/product.service';
+import { AppRoutingModule } from './app.routes';
+import { ProductService } from './services/product.service';
+import { SearchPipe } from './components/products/search-pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    SearchPipe
   ],
   imports: [
     AppRoutingModule,
